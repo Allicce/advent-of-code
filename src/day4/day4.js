c438980cb1f45009acfdd15ae4bb6fea6bbce362
allicce <stetiarova.alzbeta@gmail.com>
2021-12-04 11:52:50 +0100

    day 4 part 2

--- src/day4/day4.js
+++ src/day4/day4.js
@@ -56,6 +56,15 @@ import testFile from "../day4/test_4.txt";
 //
 // To guarantee victory against the giant squid, figure out which board will win first. What will your final score be if you choose that board?
 
+// --- Part Two ---
+// On the other hand, it might be wise to try a different strategy: let the giant squid win.
+//
+//     You aren't sure how many bingo boards a giant squid could play at once, so rather than waste time counting its arms, the safe thing to do is to figure out which board will win last and choose that one. That way, no matter which boards it picks, it will win for sure.
+//
+// In the above example, the second board is the last to win, which happens after 13 is eventually called and its middle column is completely marked. If you were to keep playing until this point, the second board would have a sum of unmarked numbers equal to 148 for a final score of 148 * 13 = 1924.
+//
+// Figure out which board will win last. Once it wins, what would its final score be?
+
 let drawnNumbers = []
 let boards = [[]]
 
@@ -81,6 +90,20 @@ const winCheck = () => {
         })
     })
     return winIndex
+}
+
+const lastWinBoard = () => {
+    let indexOfWinBoard = winCheck()
+    while(indexOfWinBoard !== null) {
+        const lastBoard = boards.splice(indexOfWinBoard, 1)
+        console.log('boards : ', boards)
+        if(boards.length === 0) {
+            boards.push(lastBoard[0])
+            return true
+        }
+        indexOfWinBoard = winCheck()
+    }
+    return false
 }
 
 export const exercise_7 = async () => {
@@ -111,13 +134,12 @@ export const exercise_7 = async () => {
                         }
                     }))
                 })
-                const isWin = winCheck()
-                console.log(isWin)
-                if(isWin !== null) {
-                    console.log('win: ', isWin)
+                const isLastWin = lastWinBoard()
+
+                if(isLastWin) {
                     console.log('boards ', boards)
 
-                    boards[isWin].forEach( row => row.forEach(item => {
+                    boards[0].forEach( row => row.forEach(item => {
                         if(item !== true) {
                             tableNumbersSum += Number(item)
                         }
