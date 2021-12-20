f682b02e5462eba78b8b956639791818a8832e2a
allicce <stetiarova.alzbeta@gmail.com>
2021-12-20 18:17:49 +0100

    day 17 part 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React, { Component } from 'react'
-import {exercise_29, exercise_30} from "./day15/day15";
+import {exercise_33} from "./day17/day17";
 
 
 
@@ -10,7 +10,7 @@ import {exercise_29, exercise_30} from "./day15/day15";
 
 class App extends Component {
     componentDidMount() {
-        exercise_30()
+        exercise_33()
     }
 
     render() {
