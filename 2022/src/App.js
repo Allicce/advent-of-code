d592c4822451294aad9f57fe6ca2f5512929bcd5
allicce <stetiarova.alzbeta@gmail.com>
2022-12-01 07:43:55 +0100

    [2022] day 1

--- 2022/src/App.js
+++ 2022/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import React, { Component } from 'react'
+import {part_one, part_two} from "./day1/day1";
 
 class App extends Component {
     componentDidMount() {
-
+        part_two()
     }
 
     render() {
